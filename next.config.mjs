6ebbfbbbb17988bb8dd2225d23d707629d16bb05
Tanush Chintala <tanushchintala@gmail.com>
2026-01-07 00:53:19 -0600

    Configure GitHub Pages deployment with GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/tanush-portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/tanush-portfolio' : '',
+  basePath: '',
+  assetPrefix: '',
 }
 
 export default nextConfig
